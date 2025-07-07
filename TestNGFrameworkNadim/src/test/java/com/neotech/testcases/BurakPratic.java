61425c1b72cb902946b28d831b9ecabe1e3ece1d
burak <burakkara0090@gmail.com>
2025-07-06 20:20:01 -0500

    nadime yolla first way

---
+++ TestNGFrameworkNadim/src/test/java/com/neotech/testcases/BurakPratic.java
@@ -0,0 +1,13 @@
+package com.neotech.testcases;
+
+public class BurakPratic {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("burak kara pratik");
+
+		//burak kara git pratik yapmaya devam et
+	}
+
+}
